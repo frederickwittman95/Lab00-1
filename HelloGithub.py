2830e84c3d6260caf68f35eef9f32a366b2afacd
frederickwittman95 <35941345+frederickwittman95@users.noreply.github.com>
2019-02-03 12:28:28 -0800

    Update HelloGithub.py

--- HelloGithub.py
+++ HelloGithub.py
@@ -1,12 +1,12 @@
-print("Hello Github!")
-file_squestns = open('StudentQuestions.txt', 'r')
-file_sanswers = open('StudentAnswers.txt', 'w')
-for line in file_squestns:
-  ans = input(line)
-  file_sanswers.write(ans + '\n') 
-print("Goodbye Github!")
-file_squestns.close()
-file_sanswers.close()
+print("Hello Github!")                             # Print the text to the console window
+file_squestns = open('StudentQuestions.txt', 'r')  # Open the file for reading
+file_sanswers = open('StudentAnswers.txt', 'w')    # Open the file for writing
+for line in file_squestns:                         # For each line in the reading file, do the following:
+  ans = input(line)                                  # 1. Provide the user with a line from the read file; assign user input to "ans"
+  file_sanswers.write(ans + '\n')                    # 2. Write the user input to the write file
+print("Goodbye Github!")                           # Print the text to the console window
+file_squestns.close()                              # Close the read file
+file_sanswers.close()                              # Close the write file
 
 
 
